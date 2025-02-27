8df91941f733755c111e198b35ad1dafa92f089b
harisankar <harisankar705@gmail.com>
2025-02-27 21:26:45 +0530

    test

--- src/server.ts
+++ src/server.ts
@@ -62,6 +62,20 @@ app.use(chatRoutes);
 app.use(errorHandler);
 
 app.use(morganMiddleware);
+app.get('/debug/uploads', (req, res) => {
+  const fs = require('fs');
+  const path = require('path');
+
+  const directoryPath = path.join(__dirname, 'utils/uploads/profile-pictures');
+  
+  fs.readdir(directoryPath, (err:any, files:any) => {
+    if (err) {
+      return res.status(500).json({ error: "Unable to scan directory" });
+    } 
+    res.json({ files });
+  });
+});
+
 
 const server = http.createServer(app);
 const io = new Server(server, {
