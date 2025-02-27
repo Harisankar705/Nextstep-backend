f7a9b6879cb153cb8bc103f093b68a3068614292
harisankar <harisankar705@gmail.com>
2025-02-27 10:45:19 +0530

    changed

--- src/server.ts
+++ src/server.ts
@@ -24,11 +24,10 @@ dbConnection();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));  
 
 // CORS 
 const allowedOrigins = [
-  'http://localhost:5173', 
   'https://nextstepbyhari.online', 
   'https://www.nextstepbyhari.online'
 ];
